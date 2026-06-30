863633eb8edb3f29ee5b93c0b451875fefc71e4a
Yanshuman Yadav <yanshuman2005@gmail.com>
2026-06-30 22:09:20 +0530

    Run BitTorrent logic in Gleam

--- .codecrafters/compile.sh
+++ .codecrafters/compile.sh
@@ -1,14 +1,7 @@
 #!/bin/sh
-#
 # This script is used to compile your program on CodeCrafters
-#
 
 set -e # Exit on failure
-
-cd gleam
-gleam build
-
-cd ..
 
 mix escript.build
 mv codecrafters_bittorrent /tmp/codecrafters-build-bittorrent-elixir
